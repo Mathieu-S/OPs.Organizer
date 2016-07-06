fd90a4089ec9026fcc6a37c4764759616e1801d0
Mathieu-S <mathieusartorio@gmail.com>
2016-07-07 00:06:42 +0200

    ajout du systeme de login et d'inscrption

--- functions/functions.php
+++ functions/functions.php
@@ -9,6 +9,46 @@ function connectionDB() {
     return $db;
 }
 
+//////////////////////////////////////
+//       Gère les utilisateurs      //
+//////////////////////////////////////
+
+/**
+ * Ajoute un utilisateur à la base de données
+ */
+function addUser() {
+    $db=connectionDB();
+    $query = $db->prepare("INSERT INTO `players`(`pseudo`, `mdp`, `email`) VALUES (:pseudo, :mdp, :email)");
+    $query->bindParam(':pseudo', $_POST['pseudo']);
+    $query->bindParam(':mdp', $_POST['mdp']);
+    $query->bindParam(':email', $_POST['email']);
+    $query->execute();
+}
+
+/**
+ * Permet de se connecter et d'initialiser une session
+ */
+function connectUser() {
+    $db = connectionDB();
+    $query = $db->prepare("SELECT * FROM `players` WHERE `pseudo` = :pseudo AND `mdp` = :mdp");
+    $query->bindParam(':pseudo', $_POST['pseudo']);
+    $query->bindParam(':mdp', $_POST['mdp']);
+    $query->execute();
+    $row = $query->fetch();
+    if($query->rowCount()) {
+        $_SESSION['id'] = $row['id'];
+        $_SESSION['pseudo'] = $row['pseudo'];
+        $_SESSION['lvlAbilitation'] = $row["lvlAbilitation"];
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//////////////////////////////////////
+//         Gère les missions        //
+//////////////////////////////////////
+
 /**
  * Récupère les missions
  */
